720d709cada236654ccdd10252d612b3ded51cd2
Anastasiia Davydova <anactaciadav.00@gmail.com>
2024-03-12 00:29:15 +0200

    first

--- PageUadreams/Locators_builtin.spec.js
+++ PageUadreams/Locators_builtin.spec.js
@@ -13,8 +13,17 @@ const { test, expect } = require('@playwright/test');
 test('Built-inLocators', async ({ page }) => {
   await page.goto('https://www.uadreams.com/');
   const girl = await page.getByAltText('girl');
+  await page.waitForTimeout(3000);
   await expect(girl).toBeVisible();
+//page.getByPlaceholder()
 
-  await page.getByPlaceholder('E-mail');
-  await page.getByRole()
+  await page.getByPlaceholder('Enter Username or E-mail').fill('123456');
+
+  //await page.getByRole()
+  await page.getByRole('button', {type: 'submit'}).click();
+  //page.getByText()
+  await expect(await page.getByText('Best ukranian ladies')).toBeVisible()
+
+  const name = await page.locator('//').textContent();
+  await expect(await page.getByText(name)).toBeVisible();
 });
